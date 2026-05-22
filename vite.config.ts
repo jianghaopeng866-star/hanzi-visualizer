8f536f8218832083a095d866b15b913ce4e686c0
jianghaopeng866-star <jianghaopeng866@gmail.com>
2026-05-22 12:12:48 +0800

    部署汉字可视化系统

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/hanzi-visualizer/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
